192cec55aec7f6d3f9ad6ea4d1296e7eb8103a6a
Krosver <kirill.kutyrev@gmail.com>
2023-02-17 18:14:30 +0300

    Update car

--- homework_02/car.py
+++ homework_02/car.py
@@ -6,9 +6,7 @@ from engine import Engine
 
 
 class Car(base.Vehicle):
-    def __init__(self, weight, fuel, fuel_consumption):
-        super().__init__(weight, fuel, fuel_consumption)
-        self.engine = None
-
+    engine = None
+    
     def set_engine(self, engine: Engine):
         self.engine = engine
